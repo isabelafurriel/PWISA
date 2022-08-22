6cc0f57f67f6d145b679b8c2e11da08374452bb3
isabelafurriel <isabela_furriel@outlook.com>
2022-08-22 15:22:55 -0300

    ATIVIDADE-AULA

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 
-<h1>hello word</h1> 
+
 <link rel="stylesheet" href="estilo.css">
+
+
 <?php 
+
 echo "Minha mensagem";
+echo"<br>";
+echo "<h1>Meu HTML</h1>";
+
+$nome = "Isabela";
+$idade = 20;
+$alturea = 1.61;
+$solteiro = true;
+
+
+
+
+$prova1 = 8;
+$prova2 = 7.5;
+$soma =$prova1 + $prova2;
+$media = $soma / 2;
+
+$multiplicacao = $prova1 + $prova2;
+$subtracao = $prova1 - $prova2;
+ 
+CONST NOMECOMPLETO = "ISABELA FURRIEL PALONE";
+echo NOMECOMPLETO; 
+
+
 ?>
