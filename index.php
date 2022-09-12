0fb88629bb3c11870cfa25788b98c6919cda4f91
isabelafurriel <isabela_furriel@outlook.com>
2022-09-12 14:31:26 -0300

    estilização da pag

--- index.php
+++ index.php
@@ -15,11 +15,45 @@
         }
         #container {
             display: grid;
-            grid-template-rows: 100px auto ;
+            grid-template-rows: 15vh  85vh;
             grid-template-areas:
                 'cabecalho'
                 'conteudo'
         }
+        #cabecalho{
+            grid-area: cabecalho;
+            background: #1d1e20;
+            color:white;
+            display: flex;
+            flex-wrap: wrap; 
+            justify-content: space-between;
+            align-items: center;
+            padding: 20px ;
+        }
+        #conteudo {
+            grid-area: conteudo;
+            background-color: #ccc;
+        }
+        img{
+            margin-top: 1px;
+            margin-left: 1px;
+        
+        }
+        .menu{
+            display: flex;
+        }
+        .menu li{
+            padding: 20px;
+            margin-left: 20px;
+        }
+        .menu li a{
+            display: block;
+            padding: 20px;
+            margin-left: 20px;
+            background-color:green;
+        }
+     
+        
      </style>
 
 </head>
@@ -27,21 +61,20 @@
     <div id="container">
         <header id="cabecalho">
         <div class="logo">
-        <img src="./logo.png">
-</div>
-<nav>
-    <ul class="menu">
-         <li>Home</li>
-         <li>Produtos</li>
-         <li>Clientes</li>
-</ul>
-<nav>
+            <img src="./logo.png" width="100" height="100"> 
+        </div>
+        <nav>
+            <ul class="menu">
+                <li>
+                <li><a>Home</a> </li>
+                <li><a>Produtos</a> </li>
+                <li><a>Clientes</a> </li>
+            </ul>
+        <nav>
 </header>
+</main id ="conteudo">
+<h1> Bem vindo a Home </h1>
 </main>
 </div>
-</header>
-    Bem vindo a Home 
-<body>
-    
 </body>
 </html>
